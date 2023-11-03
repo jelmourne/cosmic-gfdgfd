54440b5f9e998aeaebf265669365e57ef7104c92
Tristan Dasilva <67392244+tristandasilva@users.noreply.github.com>
2023-11-03 16:17:49 -0400

    Production page UI

--- View/UserPage/ProdPage.xaml.cs
+++ View/UserPage/ProdPage.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using cosmic_management_system.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,12 +16,28 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace cosmic_management_system.View.UserPage {
-    /// <summary>
-    /// Interaction logic for ProdPage.xaml
-    /// </summary>
+
     public partial class ProdPage : Page {
+        HttpClient client = new HttpClient();
         public ProdPage() {
+            client.BaseAddress = new Uri("https://localhost:7211/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json") );
             InitializeComponent();
+        }
+
+        private async void addStageBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Stage stage = new Stage();
+
+            stage.name = stageNameBox.Text;
+            stage.genre = stageGenreBox.Text;
+            stage.size = stageSizeBox.Text;
+
+            var server_response = await client.PostAsJsonAsync("AddStage", stage);
+
+            MessageBox.Show(server_response.ToString());
         }
     }
 }
