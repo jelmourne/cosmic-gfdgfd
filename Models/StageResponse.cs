d7d4b8b2d6d45ef08731bb57df67f3800cb16b39
Tristan Dasilva <67392244+tristandasilva@users.noreply.github.com>
2023-11-08 02:41:36 -0500

    Show all stages

---
+++ Models/StageResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cosmic_management_system.Models
+{
+    internal class StageResponse
+    {
+        public int status { get; set; }
+        public string message { get; set; }
+        public Stage stage { get; set; }
+        public List<Stage> stages { get; set; }
+    }
+}
